a4e05f0c1dc8879610758e9dbf71f1142ea4bde5
Aidan <ajm67@njit.edu>
2019-02-21 17:56:32 -0600

    Added styling, corrected css

--- index1.php
+++ index1.php
@@ -1,7 +1,27 @@
-<style>
-    <?php include 'main.css'; ?>
-</style>
+<html>
+<head>
+    <title>FORMATTING FOR TABLE ELEMENTS</title>
+    <style type="text/css">
+        table {
+            margin: 2px;
+        }
 
+        th {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 4em;
+            background: #666;
+            color: #FFF;
+            padding: 1px 3px;
+            border-collapse: separate;
+            border: 1px solid #000;
+        }
+
+        td {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 2em;
+            border: 2px solid #DDD;
+        }
+    </style>
 <?php
 
 $file = 'data.csv';;
@@ -23,39 +43,64 @@ class main1 {
         #this is the function that brings together the different classes
 }
 ?>
-<?php $c = 0; ?>
+
+
 <?php
+
 class csvimport {
-    static public function getRecords() {
 
+    static public function getRecords()
+    {
+        $notate = 'If this text displays, this is functioning';
+        #this is good
 
-        $notate = 'This is working - TEST';
-                            #this is good
-
-        echo "<html><body><table>\n\n";
+        echo "<html><body><table class=\"table\">\n\n";
 
         $forceopen = fopen("test.csv", "r");
-                            #r is to read
-
-        while (($contentline = fgetcsv($forceopen)) !== false) {
-
-            echo "<tr>";
-
-            foreach ($contentline as $celldata) {
-
-                echo "<td>" . htmlspecialchars($celldata) . "</td>";
-
-            }
-            echo "</tr>\n";
-        }
-
-        fclose($forceopen);
-        echo "\n</table></body></html>";
+        #r is to read
 
 
-        return $notate;
-    }
+  while (($contentline = fgetcsv($forceopen)) !== false) {
+                    $first = true;
+                   echo "<tr>";
+
+                   foreach ($contentline as $celldata) {
+                       if ($first) {
+                           echo "<th>" . htmlspecialchars($celldata) . "</th>";
+                           $first = false;
+                       }
+                       else{
+                           echo "<td>" . htmlspecialchars($celldata) . "</td>";
+
+                       }
+                       }
+                  echo "</tr>\n";
+               }
+
+
+
+              fclose($forceopen);
+              echo "\n</table></body></html>";
+
+
+              return $notate;
+           }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class html{
     static public function generateTable($notate){
@@ -76,3 +121,4 @@ class system {
 }
 
 ?>
+</html>
