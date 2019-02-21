51abf9c0ff174b9ef4a262ba98622ab4de38a8cf
Aidan <ajm67@njit.edu>
2019-02-21 16:53:51 -0600

    Added functional code, commented to make things clear, corrected errors

--- index1.php
+++ index1.php
@@ -1,13 +1,77 @@
+<style>
+    <?php include 'main.css'; ?>
+</style>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Aidan
- * Date: 2/20/2019
- * Time: 8:47 PM
- */
 
-echo "test"
+$file = 'data.csv';;
+            # this uses a local file, stored on my machine.
 
+$startprogram = new main1($file);
+main1::start();
+
+
+class main1 {
+
+    private $html;
+
+    static public function start(){
+        $notate = csvimport::getRecords();
+        $tabled = html::generateTable($notate);
+        system::printPage($tabled);
+    }
+}
+?>
+<?php $c = 0; ?>
+<?php
+class csvimport {
+    static public function getRecords() {
+
+
+        $notate = 'This is working - TEST';
+                            #this is good
+
+        echo "<html><body><table>\n\n";
+
+        $forceopen = fopen("test.csv", "r");
+                            #r is to read
+
+        while (($contentline = fgetcsv($forceopen)) !== false) {
+
+            echo "<tr>";
+
+            foreach ($contentline as $celldata) {
+
+                echo "<td>" . htmlspecialchars($celldata) . "</td>";
+
+            }
+            echo "</tr>\n";
+        }
+
+        fclose($forceopen);
+        echo "\n</table></body></html>";
+
+
+        return $notate;
+    }
+}
+
+class html{
+    static public function generateTable($notate){
+
+        $tabled = $notate;
+
+        return $tabled;
+    }
+
+}
+
+class system {
+    static public function printPage($entirepage)
+    {
+
+        echo $entirepage;
+    }
+}
 
 ?>
-
